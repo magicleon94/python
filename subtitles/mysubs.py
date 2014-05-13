6df32ee6fd211e44296c4d489003c49e3e166a3e
magicleon94 <magicleon94@gmail.com>
2014-05-13 20:16:11 +0200

    alla grezza va, da rendere umano

--- subtitles/mysubs.py
+++ subtitles/mysubs.py
@@ -1,11 +1,42 @@
 import urllib, urllib2
 import cookielib
 import re
+import zipfile
+import os,sys
+
 #stesura grezza, molto grezza
-serie_test = "revolution"
-season_test = int('02')     #trasforma '01' in '1' et similia
-episode_test = int("20")
+"""
+if len(sys.argv)<=1:
+    print "Execute with the following arguments:"
+    print "serie_name season_number episode_number directory"
+    print "edit the username and password variables to log in correctly"
+    quit()
+
+serie = sys.argv[1]
+season = int(sys.argv[2])
+episode = int(sys.argv[3])
+directory = sys.argv[4]
+tmp_file = 'tmp.zip'
+
+"""
+serie = "game of thrones"
+season = int('4') 
+episode = int("6")
 episode_range = (1,14)
+tmp_file= 'tmp.zip'
+directory = './'
+
+username = 'username_here'
+password = 'password_here'
+#login
+login_url = 'http://www.italiansubs.net/index.php'
+login_data = {'username':username,'passwd':password,'remember':'yes', 'option':'com_user','task':'login','silent':'true','return':'aHR0cDovL3d3dy5pdGFsaWFuc3Vicy5uZXQv','4e48ce6d96cc53787c1154ca49da26b8':'1'}
+datas = urllib.urlencode(login_data)
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+page = opener.open(login_url,datas)
+page.close()
+#loggato
 
 url = urllib.urlopen('http://www.italiansubs.net/index.php?option=com_remository&Itemid=9#482')
 page = url.read()
@@ -14,48 +45,60 @@ url.close()
 match = re.search('</tr></thead><tbody>(.*?)</body>',page,re.DOTALL)
 match = re.findall('<a href="(.*?)"> (.*?)</a>',match.group(1),re.DOTALL)
 serieslist = {}
-for serie in match:
-    serieslist[serie[1].lower()] = serie[0]
+for x in match:
+    serieslist[x[1].lower()] = x[0]
 
-if not (serie_test in serieslist):
+if not (serie in serieslist):
     raise "Serie non trovata"
-url = urllib.urlopen(serieslist[serie_test])
+url = opener.open(serieslist[serie])
 page = url.read()
 url.close()
+
 #ricerca stagione
 match = re.search('</tr></thead><tbody>(.*?)</tbody>',page,re.DOTALL)
-match = re.findall('<a href="(.*?)> (.*?)</a>',match.group(1),re.DOTALL)
+match = re.findall('<a href="(.*?)> Stagione (.*?)</a>',match.group(1),re.DOTALL)
 seasonlist = {}
-season_number = 1
-for season in match:
-    seasonlist[season_number]=match[season_number-1][0]
-    season_number+=1
-if not (season_test in seasonlist):
-        raise "Stagione non trovata"
+for x in match:
+    seasonlist[int(x[1])] = x[0]
+if not (season in seasonlist):
+    raise "Stagione non trovata"
 
 #ricerco episodi
-url = urllib.urlopen(seasonlist[season_test])
+url = opener.open(seasonlist[season])
 page = url.read()
 url.close()
-
 match = re.search("<div id='remositoryfilelisting'>(.*?)<!-- End of remositoryfilelisting -->",page,re.DOTALL)
-match = re.findall(';<a href="(.*?)">'+ str(serie_test) + ' ' + str(season_test) + 'x'+'(.*?)</a>',match.group(1),re.DOTALL | re.IGNORECASE)
-
+match = re.findall(';<a href="(.*?)">'+ str(serie) + ' ' + str(season) + 'x'+'(.*?)</a>',match.group(1),re.DOTALL | re.IGNORECASE)
 episodelist = {}
-episode_number = 1
-for ep in match:
-    episodelist[episode_number]=match[episode_number-1][0]
-    episode_number+=1
-if not (episode_test in episodelist):
-    raise "Episodio non trovato"
-
-url = urllib.urlopen(episodelist[episode_test])
+for x in match:
+    if len(x[1])==2:
+        episodelist[int(x[1])]=x[0]
+if not (episode in episodelist):
+    raise ("Episodio non trovato")
+#apro la pagina del sottotitolo
+url = opener.open(episodelist[episode])
 page = url.read()
 url.close()
 
-#match = re.search('<h2><br /><center>'+str(serie_test)+' '+str(season_test)+'x'+'(.*?) </center></h2><dt><br /><center><a href="(.*?)"',page,re.DOTALL)
+match = re.search('<!-- End of remositorypathway-->(.*?)<!-- End of remositoryfileinfo -->',page,re.DOTALL)
+match = re.findall(serie+'(.*?)</center></h2><dt><br /><center><a href="(.*?)" rel="nofollow">',match.group(1),re.DOTALL | re.IGNORECASE)
 
-urllib.urlretrieve('http://www.italiansubs.net/index.php?option=com_remository&Itemid=6&func=download&id=50580&chk=db89b463945dd4c3c5ad94f3eefd9af4&no_html=1','prova')
+download_url = match[0][1]
+download_page = opener.open(download_url)
+zippy = download_page.read()
+download_page.close()
+zipped_sub = open(tmp_file,'wb')
+zipped_sub.write(zippy)
+zipped_sub.close()
 
-    
+zipped_sub = open(tmp_file)
+zipobj = zipfile.ZipFile(zipped_sub)
+zipobj.extractall(directory)
+zipobj.close()
+zipped_sub.close()
+os.remove(tmp_file)
+  
+
+print 'ok'
+
 
